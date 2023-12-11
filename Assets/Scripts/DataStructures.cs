b909c1eaa010e55468af485f095be6fbf71f77f7
John Winski <JWINSKI@nait.ca>
2023-12-11 14:35:56 -0700

    Add project files.

---
+++ Assets/Scripts/DataStructures.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct CharacterData
+{
+    string char_Name;
+    int char_ID;
+    int char_STR;
+    int char_DEX;
+    int char_CON;
+    int char_Model;
+}
